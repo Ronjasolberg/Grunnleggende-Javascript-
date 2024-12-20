c758cc578f455026586e85a4f3bea8c9a5be8d29
Ronjasolberg <ronjasolberg12@hotmail.com>
2024-12-20 13:05:03 +0100

    light-theme and dark-theme implementation

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log('Hello!');
+//hent knappen og body-elementet 
+const themeSwitcher = document.getElementById("theme-switcher"); 
+const body = document.body; 
+
+//Funksjoner som bytter tema 
+function toggleTheme () {
+    //Bytt mellom lys og mørk tema 
+    body.classList.toggle("light-theme");
+    body.classList.toggle("dark-theme");
+}
+
+//Event listener på knappen 
+themeSwitcher.addEventListener("click", toggleTheme);
